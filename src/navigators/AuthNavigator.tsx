448f2f9ed0eb575911cd7caa9a7d04b2c1f72c77
Adwait Narayan Pradhan <adwaitpradhan2001@gmail.com>
2024-01-28 19:53:06 +0530

    Project Setup

---
+++ src/navigators/AuthNavigator.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const AuthNavigator = () => {
+  return (
+    <View>
+      <Text>AuthNavigator</Text>
+    </View>
+  );
+};
+
+export default AuthNavigator;
+
+const styles = StyleSheet.create({});
---
+++ src/navigators/index.tsx
@@ -0,0 +1,11 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const MainNavigator = () => {
+  return <NavigationContainer>{}</NavigationContainer>;
+};
+
+export default MainNavigator;
+
+const styles = StyleSheet.create({});
